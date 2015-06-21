ceb915736bf3ff24a62e5cacc1aecf59f490137a
Sebastian <sebastianls0228@gmail.com>
2015-06-21 02:31:52 -0500

    Almost Ready, added paneles image

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
+
 $(document).foundation();
+
+$(function() {
+	$('a[href*="panel"]').click(function() {
+    location.replace("#productos")
+});
+});
